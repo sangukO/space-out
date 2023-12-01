9b0ffea6422871f36e6a84d883e303882445a712
sangukO <woqls1972@gmail.com>
2023-12-01 18:00:33 +0900

    feat : 캔버스 추가

--- src/pages/FireworkPage.tsx
+++ src/pages/FireworkPage.tsx
@@ -1,6 +1,16 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
-function Firework() {
+interface CanvasProps {
+    width: number;
+    height: number;
+}
+
+function Firework({
+    width = window.innerWidth - 50,
+    height = window.innerHeight - 50,
+}: CanvasProps) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     type Props = {
         symbol: string;
         label: string;
@@ -17,15 +27,46 @@ function Firework() {
         </span>
     );
 
+    const draw = () => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const context = canvas.getContext("2d");
+        if (!context) {
+            return;
+        }
+        context.beginPath();
+        context.moveTo(150, 150);
+        context.arc(150, 150, 100, 0, (Math.PI / 180) * 90, true);
+        context.closePath();
+        context.stroke();
+
+        context.beginPath();
+        context.strokeStyle = "blue";
+        context.moveTo(150, 150);
+        context.arc(150, 150, 100, 0, (Math.PI / 180) * 90, false);
+        context.closePath();
+        context.fill();
+    };
+    draw();
     return (
         <>
             <div
                 className="backgroud firework-background"
-                style={{ color: "white" }}
+                style={{ color: "white", justifyContent: "center" }}
             >
                 <Link className="text-link" to={"/"}>
                     <Emoji symbol="🌏️" label="main" />
                 </Link>
+                <div>
+                    <canvas
+                        ref={canvasRef}
+                        width={width}
+                        height={height}
+                        className="canvas"
+                    />
+                </div>
             </div>
         </>
     );
