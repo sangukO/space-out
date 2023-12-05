80c6d36b0948811ef02b74e9d9cc4b2b6896c2fc
sangukO <woqls1972@gmail.com>
2023-12-05 11:13:07 +0900

    style : css 수정

--- src/pages/ScrollPage.tsx
+++ src/pages/ScrollPage.tsx
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Fireworks } from "fireworks-js";
 
 function Firework() {
     type Props = {
@@ -18,38 +16,6 @@ function Firework() {
             {symbol}
         </span>
     );
-
-    useEffect(() => {
-        const options = {
-            rocketsPoint: {
-                min: 50,
-                max: 50,
-            },
-            intensity: 8,
-            speed: 1,
-            autoresize: true,
-            acceleration: 1.0,
-            traceSpeed: 6,
-            delay: {
-                min: 100,
-                max: 100,
-            },
-            decay: {
-                min: 0.01,
-                max: 0.05,
-            },
-            flickering: 0.0,
-            particles: 150,
-            opacity: 1.0,
-        };
-
-        const fireworks = new Fireworks(
-            document.querySelector(".Firework")!,
-            options
-        );
-        fireworks.start();
-    }, []);
-
     return (
         <>
             <div
@@ -71,9 +37,6 @@ function Firework() {
                 >
                     <Emoji symbol="🌏️" label="main" />
                 </Link>
-                <div className="FireworkWrap">
-                    <div className="Firework"></div>
-                </div>
             </div>
         </>
     );
